26df51c368369eb8bc60b1f02ae8473dac7bec2c
Takashi Nishikawa <program.takashi@gmail.com>
2019-09-01 22:10:42 +0900

    [add] create basse for chatapp by js
    reffrence : dotinstall

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC5i_4dP-S2nq7_-N8jsC_i5JXr0yPtatg",
+    authDomain: "chattry-223c0.firebaseapp.com",
+    databaseURL: "https://chattry-223c0.firebaseio.com",
+    projectId: "chattry-223c0",
+    storageBucket: "chattry-223c0.appspot.com",
+    messagingSenderId: "701860996873",
+    appId: "1:701860996873:web:5ad6a7a3423788c0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+  const collection = db.collection('messages');
+
+  collection.add({
+    messsage: 'test'
+  }).then(doc=>{
+    console.log(`${doc.id} added!`);
+  }).catch(error => {
+    console(error);
+  });
